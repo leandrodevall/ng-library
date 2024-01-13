e3eb46e5c7150fe99b43167d3872980ef2e59b3c
Leandro Souza <leandrodevall@gmail.com>
2024-01-13 16:38:58 -0300

    feat: 🎸 consum library in showcase app

--- lib-release/projects/release-me-showcase/src/app/app.component.ts
+++ lib-release/projects/release-me-showcase/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReleaseMeComponent } from '../../../release-me/src/public-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReleaseMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- lib-release/projects/release-me/src/public-api.ts
+++ lib-release/projects/release-me/src/public-api.ts
@@ -1,6 +1,1 @@
-/*
- * Public API Surface of release-me
- */
-
-export * from './lib/release-me.service';
 export * from './lib/release-me.component';
